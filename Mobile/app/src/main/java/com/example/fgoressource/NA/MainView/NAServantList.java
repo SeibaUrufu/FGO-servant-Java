598342ed6618e25c5b331c1ff6344640f003149b
Nicolas GUYOMARCH <nicolasguyomarch67@gmail.com>
2022-04-20 19:04:18 +0200

    [NA/List] Added design for the rarity and class filter. TODO: SearchView for the name and get info to update with the selected filter.

--- Mobile/app/src/main/java/com/example/fgoressource/NA/MainView/NAServantList.java
+++ Mobile/app/src/main/java/com/example/fgoressource/NA/MainView/NAServantList.java
@@ -3,8 +3,12 @@ package com.example.fgoressource.NA.MainView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,10 +19,15 @@ import com.example.fgoressource.VolleyRequestQueue;
 
 public class NAServantList extends AppCompatActivity implements IServant{
 
+    private Boolean optionsOn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.servant_list);
+
+        Button options = (Button) findViewById(R.id.searchNameButton);
+        FragmentContainerView optionsContainer = (FragmentContainerView) findViewById(R.id.optionsFragContainer);
 
         VolleyRequestQueue.getInstance(this);
 
@@ -33,6 +42,28 @@ public class NAServantList extends AppCompatActivity implements IServant{
             _recyclerView.setLayoutManager(layoutManager);
             _recyclerView.setAdapter(_adapter);
             _adapter.setOnItemClickListener(this);
+        });
+
+        options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(optionsOn) {
+                    optionsOn = false;
+                    options.setText("↓");
+                    optionsContainer.setVisibility(View.INVISIBLE);
+                } else {
+                    optionsOn = true;
+                    optionsContainer.setVisibility(View.VISIBLE);
+                    options.setText("↑");
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .setReorderingAllowed(true)
+                            .replace(R.id.optionsFragContainer, OptionsFilter.class, null)
+                            .addToBackStack("true")
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                            .commit();
+                }
+            }
         });
 
     }
---
+++ Mobile/app/src/main/java/com/example/fgoressource/NA/MainView/OptionsFilter.java
@@ -0,0 +1,26 @@
+package com.example.fgoressource.NA.MainView;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fgoressource.R;
+
+public class OptionsFilter extends Fragment {
+
+    public OptionsFilter() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_options_filter, container, false);
+    }
+}
